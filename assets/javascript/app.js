69058a4c16632f674f8d895c93adc0009bfe8258
Eyad Qassem <eyadqassem@Eyads-MacBook-Air.local>
2017-04-14 23:41:10 -0400

    updated CSS styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,82 +1,3 @@
-// //FACEBOOK INTEGRATION
-// //====================================================================================
-// function statusChangeCallback(response) {
-//   console.log('statusChangeCallback');
-//   console.log(response);
-//   // The response object is returned with a status field that lets the
-//   // app know the current login status of the person.
-//   // Full docs on the response object can be found in the documentation
-//   // for FB.getLoginStatus().
-//   if (response.status === 'connected') {
-//     // Logged into your app and Facebook.
-//     testAPI();
-//   } else {
-//     // The person is not logged into your app or we are unable to tell.
-//     document.getElementById('status').innerHTML = 'Please log ' +
-//       'into this app.';
-//   }
-// }
-//
-// // This function is called when someone finishes with the Login
-// // Button.  See the onlogin handler attached to it in the sample
-// // code below.
-// function checkLoginState() {
-//   FB.getLoginStatus(function(response) {
-//     statusChangeCallback(response);
-//   });
-// }
-//
-// window.fbAsyncInit = function() {
-// FB.init({
-//   appId      : '1678163075543673',
-//   cookie     : true,  // enable cookies to allow the server to access
-//                       // the session
-//   xfbml      : true,  // parse social plugins on this page
-//   version    : 'v2.8' // use graph api version 2.8
-// });
-//
-// // Now that we've initialized the JavaScript SDK, we call
-// // FB.getLoginStatus().  This function gets the state of the
-// // person visiting this page and can return one of three states to
-// // the callback you provide.  They can be:
-// //
-// // 1. Logged into your app ('connected')
-// // 2. Logged into Facebook, but not your app ('not_authorized')
-// // 3. Not logged into Facebook and can't tell if they are logged into
-// //    your app or not.
-// //
-// // These three cases are handled in the callback function.
-//
-// FB.getLoginStatus(function(response) {
-//   statusChangeCallback(response);
-// });
-//
-// };
-//
-// // Load the SDK asynchronously
-// (function(d, s, id) {
-//   var js, fjs = d.getElementsByTagName(s)[0];
-//   if (d.getElementById(id)) return;
-//   js = d.createElement(s); js.id = id;
-//   js.src = "https://connect.facebook.net/en_US/sdk.js";
-//   fjs.parentNode.insertBefore(js, fjs);
-// }(document, 'script', 'facebook-jssdk'));
-//
-// // Here we run a very simple test of the Graph API after login is
-// // successful.  See statusChangeCallback() for when this call is made.
-// function testAPI() {
-//   console.log('Welcome!  Fetching your information.... ');
-//   FB.api('/me', function(response) {
-//     console.log(response);
-//     console.log('Successful login for: ' + response.name);
-//     document.getElementById('status').innerHTML =
-//       'Thanks for logging in, ' + response.name + '!';
-//       userName = response.name.split(/[ ,]+/);
-//       userName2 = userName[0];
-//   });
-// }
-//
-
 // GLOBAL VARIABLES
 // =====================================================================================
 var userName2 = "";
@@ -98,31 +19,15 @@ var lovePhotoDiv;
 // FUNCTIONS
 // =====================================================================================
 // Opening screen of app - asks user to input their location
-function validation(userLocation) {
-   if (userLocation.length === 5) {
-        for( var i = 0; i < 5; i++) {
-          if (userLocation.charCodeAt(i) >= 48 && userLocation.charCodeAt(i) <= 57) {
-            console.log("it is a zip code");
-        }// end if
-        else if ((userLocation.charCodeAt(i) >= 65 && userLocation.charCodeAt(i) <= 90) || (userLocation.charCodeAt(i) >= 97 && userLocation.charCodeAt(i) <= 121) ){
-          console.log("It is a city");
-        } // end else of
-        else {
-          console.log("it is not a zip code or a city");
-          break;
-        }// end else
-      }//end for
-    }// end if
-}// end function
 
 // Initialize Firebase
- var config = {
-   apiKey: "AIzaSyDSBv8dJ2nzh0mfhCLEoba_wioPc2FfqcA",
-   authDomain: "contact-us-cb646.firebaseapp.com",
-   databaseURL: "https://contact-us-cb646.firebaseio.com",
-   projectId: "contact-us-cb646",
-   storageBucket: "contacta-us-cb646.appspot.com",
-   messagingSenderId: "73152289846"
+  var config = {
+    apiKey: "AIzaSyD22UVJI9nrCZcqfxaLg53bOvnc-xKfwZM",
+    authDomain: "food-mood-app.firebaseapp.com",
+    databaseURL: "https://food-mood-app.firebaseio.com",
+    projectId: "food-mood-app",
+    storageBucket: "food-mood-app.appspot.com",
+    messagingSenderId: "391107933257"
  };
  firebase.initializeApp(config);
 
@@ -155,14 +60,6 @@ $("#submit-msg").on("click", function(event) {
  return false;
 });
 
-$("input").keyup(function() {
-     if ($(".form-control").val() !== "") {
-       $("#home-screen-submit").removeAttr("disabled");
-     }
-     else {
-       $("#home-screen-submit").attr("disabled","disabled");
-     }
-   });
 
 // showing contact us form
 function div_show() {
@@ -175,20 +72,54 @@ function div_hide() {
    }
 
 function homeScreen() {
-  var openingGreeting = $("<div>");
-  openingGreeting.html("<h1 id ='opening-greeting'> What are you in the <span id='mood-text2'><i> mood </i></span> for?</h1>");
+  var facebookStatusDiv = $("<div>");
+  facebookStatusDiv.attr("class", "row");
+  var facebookStatus = $("<p>");
+  facebookStatus.attr("id", "status");
+  facebookStatus.attr("class", "col-lg-2 offset-lg-10");
+  facebookStatusDiv.append(facebookStatus);
 
-  var locationForm = $("<form>");
-  locationForm.attr("id", "location-form");
-  locationForm.html("<input class='form-control' id='user-location' type='text' name='user-location' placeholder='Enter your address to get started!'/>");
+  var openingGreetingDiv = $("<div>");
+  openingGreetingDiv.attr("class", "row");
+  var openingGreeting = $("<h1>");
+  openingGreeting.attr("id", "opening-greeting");
+  openingGreeting.attr("class", "col-xl-8 offset-xl-2");
+  openingGreeting.html("What are you in the <span id='mood-text2'><i> mood </i></span> for?");
+  openingGreetingDiv.append(openingGreeting);
+
+
+  var facebookGreetingDiv = $("<div>");
+  facebookGreetingDiv.attr("class", "row");
+  var facebookGreeting = $("<h4>");
+  facebookGreeting.attr("id", "facebook-greeting");
+  facebookGreeting.attr("class", "col-xl-6 offset-xl-3");
+  facebookGreeting.html("Please login to facebook to personalize user experience");
+  facebookGreetingDiv.append(facebookGreeting);
+
+  var locationFormContainer = $("<div>");
+  locationFormContainer.attr("class", "row");
+  var locationFormDiv = $("<div>");
+  locationFormDiv.attr("class", "col-xl-6 offset-xl-3");
+  var locationForm = $("<div>");
+  locationForm.attr("class", "input-group");
+  locationForm.html("<input class='form-control input-lg' id='user-location' type='text' name='user-location' placeholder='Enter your address to get started!'/>");
+  var homeScreenSubmitContainer = $("<span>");
+  homeScreenSubmitContainer.attr("class", "input-group-btn");
+
   var homeScreenSubmit = $("<button>");
-  homeScreenSubmit.attr("class", "btn btn-default");
-  homeScreenSubmit.attr("type", "submit");
+  homeScreenSubmit.attr("type", "button");
+  homeScreenSubmit.attr("class", "btn btn-secondary");
   homeScreenSubmit.attr("id", "home-screen-submit");
   homeScreenSubmit.html("Submit");
-  $("#main-section").append(locationForm);
-  $("#main-section").append(openingGreeting);
-  $("#location-form").append(homeScreenSubmit);
+  homeScreenSubmitContainer.append(homeScreenSubmit);
+  locationForm.append(homeScreenSubmit);
+  locationFormDiv.append(locationForm);
+  locationFormContainer.append(locationFormDiv);
+
+  $("#main-section").append(facebookStatusDiv);
+  $("#main-section").append(openingGreetingDiv);
+  $("#main-section").append(facebookGreetingDiv);
+  $("#main-section").append(locationFormContainer);
 }
 
 // Screen opened after the user inputs their location, lists cuisines types for the user to
@@ -203,25 +134,36 @@ function openScreen() {
     businessReviewCount: [],
   };
 
+  var cuisineTypeDiv = $("<div>");
+  cuisineTypeDiv.attr("class", "row");
 
-  var cuisineType = $("<div class='cuisine-type'>");
-  cuisineType.html("<h1 id='cuisine-header' class='cuisine-type'> What type of cuisine " + userName2 + "? </h1>");
-  $("#main-section").append(cuisineType);
+  var cuisineTypeCol = $("<div>");
+  cuisineTypeCol.attr("class", "form-check cuisine-type col-lg-4 offset-lg-4 col-xl-6 offset-xl-3");
+  cuisineTypeCol.attr("id", "cuisine-type");
+
+  var cuisineTypeHeading = $("<h1>");
+  cuisineTypeHeading.attr("id", "cuisine-header");
+  cuisineTypeHeading.html("What type of cuisine " + userName2 + "?");
+  cuisineTypeCol.append(cuisineTypeHeading);
+
   var foodTypes = ["Italian", "Chinese", "Mediterranean", "Mexican", "Indian", "Sushi"];
   var counter = 1;
   for(var i = 0; i < foodTypes.length; i++) {
     var foodList = $("<label>");
-    foodList.attr("class", "food-list radio");
+    foodList.attr("class", "food-list form-check-label control-label col-lg-12");
     foodList.attr("id", "food-list" + counter);
-    foodList.html("<input value=" + foodTypes[i] + " " + "type='radio' name='optradio' class='food-value'>" + foodTypes[i]);
-    $("#cuisine-header").append(foodList);
+    foodList.html("<input value=" + foodTypes[i] + " " + "type='radio' name='optradio' class=' form-'check-input'>" + foodTypes[i]);
+    cuisineTypeCol.append(foodList);
     counter++;
   }
 
   var getStarted = $("<p>");
   getStarted.attr("id", "get-started");
   getStarted.html("<a id='get-started-text'>Submit</a>");
-  $("#cuisine-header").append(getStarted);
+
+  cuisineTypeCol.append(getStarted);
+  cuisineTypeDiv.append(cuisineTypeCol);
+  $("#main-section").append(cuisineTypeDiv);
 }
 
 // Pulls photos from the Yelp API based on the user's location and desired cuisine type
@@ -342,15 +284,30 @@ function yelpSearch() {
 function showPhoto() {
   $("#main-section").empty();
 
+  var showPhotoContainer = $("<div>");
+  showPhotoContainer.attr("class", "row");
+  showPhotoContainer.attr("id", "show-photo-container");
+
+  var showPhotoDiv = $("<div>");
+  showPhotoDiv.attr("id", "show-photo-div");
+  showPhotoDiv.attr("class", "col-xl-6 offset-xl-3");
+
+  var foodImagesContainer = $("<div>");
+  foodImagesContainer.attr("id", "food-images");
+  foodImagesContainer.attr("class", "row well well-lg");
+
   var foodImagesDiv = $("<div>");
-  foodImagesDiv.attr("id", "food-images");
-  $("#main-section").append(foodImagesDiv);
+  foodImagesDiv.attr("id", "food-images-div");
+  foodImagesDiv.attr("class", "col-xl-8 offset-xl-2");
 
   var foodImage = $("<img>");
   foodImage.attr("id", "food-img");
-  foodImage.attr("class", "well well-lg");
+  // foodImage.attr("class", "well well-lg");
   foodImage.attr("src", businessInfo.businessImages[imageCount]);
-  $("#food-images").append(foodImage);
+  foodImagesDiv.append(foodImage);
+  foodImagesContainer.append(foodImagesDiv);
+  showPhotoDiv.append(foodImagesContainer);
+
   console.log(businessInfo.businessAddress[imageCount]);
 
   // Adding Yelp logo/link to Yelp to image in order to comply with Yelp API display requirements
@@ -362,15 +319,20 @@ function showPhoto() {
   yelpLogo.attr("src", "assets/images/Yelp_trademark_RGB_outline.png");
   yelpLogo.attr("alt", "Yelp Logo");
   yelpLink.append(yelpLogo);
-  $("#food-images").append(yelpLink);
+  foodImagesDiv.append(yelpLink);
 
   // Creating like/dislike "buttons" as images with Bootstrap img-rounded class
   // Need to add on-click event listener and cursor hover event
-  var buttonsDiv = $("<div>");
-  buttonsDiv.attr("id", "buttons-div");
+  var buttonsContainer = $("<div>");
+  buttonsContainer.attr("class" ,"row");
+
 
   // Creating like & dislike "buttons" as images with Bootstrap img-rounded class
   // **Need to add on-click event listener for both buttons**
+  var buttonsDiv = $("<div>");
+  buttonsDiv.attr("class", "col-xl-10 offset-xl-2");
+  buttonsDiv.attr("id", "like-buttons-div");
+
   var dislikeButton = $("<img>");
   dislikeButton.addClass("img-rounded");
   dislikeButton.attr("id", "dislike-btn");
@@ -382,8 +344,13 @@ function showPhoto() {
   likeButton.attr("id", "like-btn");
   likeButton.attr("src", "assets/images/like-button2.png");
   buttonsDiv.append(likeButton);
-  $("#main-section").append(buttonsDiv);
 
+  buttonsContainer.append(buttonsDiv);
+  showPhotoDiv.append(buttonsContainer);
+
+
+  showPhotoContainer.append(showPhotoDiv);
+  $("#main-section").append(showPhotoContainer);
 }
 
 function nextPhoto() {
@@ -400,18 +367,31 @@ function nextPhoto() {
 }
 
 function lovePhoto() {
-  $("#like-btn").hide();
-  $("#dislike-btn").hide();
-  $("#food-images").hide();
+  $("#show-photo-container").hide();
+
+  lovePhotoContainer = $("<div>");
+  lovePhotoContainer.attr("class", "row");
 
   lovePhotoDiv = $("<div>");
+  lovePhotoDiv.attr("class", "col-xl-6 offset-xl-3");
   lovePhotoDiv.attr("id", "love-photo");
 
+  var yelpInfoContainer = $("<div>");
+  yelpInfoContainer.attr("class", "yelp-info-div");
   var yelpInfoDiv = $("<div>");
-  yelpInfoDiv.attr("class", "yelp-info-div");
+  yelpInfoDiv.attr("class", "row");
 
-
+ //add columns and rows for encompassing love-photo
+  var businessDisplayDiv = $("<div>");
+  businessDisplayDiv.attr("class", "col-xl-12");
   var businessDisplay = $("<h1>").html(businessInfo.businessName[imageCount]);
+  businessDisplay.attr("id", "business-display");
+
+  businessDisplayDiv.append(businessDisplay);
+  yelpInfoDiv.append(businessDisplayDiv);
+  yelpInfoContainer.append(yelpInfoDiv);
+  lovePhotoDiv.append(yelpInfoContainer);
+
   var ratingImage = $("<img>");
   ratingImage.attr("id", "yelp-rating");
   ratingImage.attr("src", businessInfo.businessRating[imageCount]);
@@ -424,39 +404,64 @@ function lovePhoto() {
   yelpLogo2.attr("alt", "Yelp Logo");
   yelpLogo2.attr("id", "yelp-logo-2");
   yelpLink2.append(yelpLogo2);
+
+  var ratingDisplayDiv = $("<div>");
+  ratingDisplayDiv.attr("id", "rating-display-div");
+  ratingDisplayDiv.attr("class", "row");
   var ratingDisplay = $("<h2>");
+  ratingDisplay.attr("id", "yelp-logo-rating")
+  ratingDisplay.attr("class", "col-xl-6");
   ratingDisplay.append(ratingImage);
   ratingDisplay.append(yelpLink2);
+
+
   var reviewCount = businessInfo.businessReviewCount[imageCount];
   var reviewCountDisplay = $("<h3>").html("Based on " + reviewCount + " reviews");
+  reviewCountDisplay.attr("class", "col-xl-6");
   reviewCountDisplay.attr("id", "review-count-display");
+  ratingDisplayDiv.append(reviewCountDisplay);
+  ratingDisplayDiv.append(ratingDisplay);
 
-  yelpInfoDiv.append(businessDisplay);
-  yelpInfoDiv.append(ratingDisplay);
-  yelpInfoDiv.append(reviewCountDisplay);
-  lovePhotoDiv.append(yelpInfoDiv);
-
-  var chooseAnother = $("<div>");
-  chooseAnother.attr("id", "chooseAnother");
-  chooseAnother.text("Still not satisfied?");
-  lovePhotoDiv.append(chooseAnother);
-
-  var returnButton = $("<button>");
-  returnButton.attr("id", "return-btn");
-  returnButton.attr("class", "btn btn-default");
-  returnButton.attr("type", "submit");
-  returnButton.html("Choose another restaurant");
-  lovePhotoDiv.append(returnButton);
-
-  var startOverButton = $("<button>").text("Pick a New Cuisine");
-  startOverButton.addClass("btn btn-default");
-  startOverButton.attr("id", "start-over-btn");
-  lovePhotoDiv.append(startOverButton);
+  yelpInfoDiv.append(ratingDisplayDiv);
+  yelpInfoContainer.append(yelpInfoDiv);
+  lovePhotoDiv.append(yelpInfoContainer);
 
   // returns getDirections() which triggers google API directions search and embed
   getDirections();
 
-  $("#main-section").append(lovePhotoDiv);
+  var chooseAnotherContainer = $("<div>");
+  chooseAnotherContainer.attr("class", "row");
+  var chooseAnotherDiv = $("<div>");
+  chooseAnotherDiv.attr("class", "col-xl-12");
+  chooseAnotherDiv.attr("id", "not-satisfied");
+  chooseAnotherDiv.text("Still not satisfied?");
+  chooseAnotherContainer.append(chooseAnotherDiv);
+  lovePhotoDiv.append(chooseAnotherContainer);
+
+  var buttonsContainer = $("<div>");
+  buttonsContainer.attr("class", "row");
+
+  var buttonsDiv = $("<div>");
+  buttonsDiv.attr("class", "col-xl-12");
+  buttonsDiv.attr("id", "buttons-div");
+
+  var returnButton = $("<button>");
+  returnButton.attr("id", "return-btn");
+  returnButton.attr("class", "btn btn-default center-block");
+  returnButton.attr("type", "submit");
+  returnButton.html("Choose another restaurant");
+  buttonsDiv.append(returnButton);
+
+  var startOverButton = $("<button>").text("Pick a New Cuisine");
+  startOverButton.addClass("btn btn-default center-block");
+  startOverButton.attr("id", "start-over-btn");
+  buttonsDiv.append(startOverButton);
+  buttonsContainer.append(buttonsDiv);
+
+  lovePhotoDiv.append(buttonsContainer);
+
+  lovePhotoContainer.append(lovePhotoDiv);
+  $("#main-section").append(lovePhotoContainer);
 }
 
 function returnScreen() {
@@ -464,10 +469,8 @@ function returnScreen() {
   // 1) need to hide lovePhoto screen and return-btn 2) unhide #like-btn, #dislike-btn and #food-images div
   // ensure functionality between screens and nextPhoto and lovePhoto functions on button clicks after returning
   $("#love-photo").hide();
-  $("#return-btn").hide();
-  $("#like-btn").show();
-  $("#dislike-btn").show();
-  $("#food-images").show();
+
+  $("#show-photo-container").show();
 }
 
 // Uses Google Maps Embed API to display directions from the user's current location, captured on 1st submit button click,
@@ -478,17 +481,21 @@ function getDirections() {
   var destination = businessInfo.businessAddress[imageCount];
   var queryURL = "https://www.google.com/maps/embed/v1/directions?key=" + apiKey +
     "&origin=" + origin + "&destination=" + destination;
+  var mapDisplayContainer = $("<div>");
+  mapDisplayContainer.attr("class", "row");
+  var mapDisplayDiv = $("<div>");
+  mapDisplayDiv.attr("class", "col-xl-11 offset-xl-1");
   var mapDisplay = $("<iframe>");
   // bootstrap class to create a encompassing panel around an element
-  mapDisplay.attr("class", "well well-lg");
+  mapDisplay.attr("class", "well");
   // added Id to allow for positioning of iframe
   mapDisplay.attr("id", "google-maps");
   mapDisplay.attr("src", queryURL);
-  mapDisplay.attr("width", "500");
-  mapDisplay.attr("height", "400");
   mapDisplay.attr("frameborder", "0");
   mapDisplay.attr("style", "border:0");
-  lovePhotoDiv.append(mapDisplay);
+  mapDisplayDiv.append(mapDisplay);
+  mapDisplayContainer.append(mapDisplayDiv);
+  lovePhotoDiv.append(mapDisplayContainer);
 }
 
 // MAIN PROCESS
@@ -506,7 +513,6 @@ homeScreen();
 $(document).on("click", "#home-screen-submit", function(event) {
   event.preventDefault();
   userLocation = $("#user-location").val().trim();
-  validation(userLocation);
   $("#user-location").val("");
   console.log(userLocation);
   $("#main-section").empty();
